4a779607e8682b0b790daf8a9ea8e8d55d8550ce
Afsalms <afsalms93.mec@gmail.com>
2018-12-19 21:22:02 +0530

    Completed the fetching data from the server

--- app/src/main/java/com/example/qbuser/retrofittutorial/MainActivity.kt
+++ app/src/main/java/com/example/qbuser/retrofittutorial/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.qbuser.retrofittutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.qbuser.retrofittutorial.models.Post
 import com.example.qbuser.retrofittutorial.retrofit.RetrofitService
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.internal.schedulers.IoScheduler
 import io.reactivex.schedulers.Schedulers
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -20,21 +25,25 @@ class MainActivity : AppCompatActivity() {
 
 
         val retrofit = Retrofit.Builder()
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl("http://jsonplaceholder.typicode.com/")
             .build()
 
         val a = retrofit.create(RetrofitService::class.java)
-        val result = a.getAllPosts()
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
-            .subscribe ({
-                    result ->
-                   println("Result There are ${result.items.size} Java developers in Lagos")
-            }, { error ->
-                println(error.printStackTrace())
-            })
+
+        val response = a.getAllPosts()
+        response.enqueue(object : Callback<List<Post>>{
+            override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Added", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+                val data = response!!.body()
+                data!!.forEach { println(it.title)
+                                println("-----------------------")}
+                }
+        })
+
 
 
 
@@ -43,3 +52,4 @@ class MainActivity : AppCompatActivity() {
 
     }
 }
+
--- app/src/main/java/com/example/qbuser/retrofittutorial/retrofit/Service.kt
+++ app/src/main/java/com/example/qbuser/retrofittutorial/retrofit/Service.kt
@@ -1,12 +1,13 @@
 package com.example.qbuser.retrofittutorial.retrofit
 
 import com.example.qbuser.retrofittutorial.models.Post
+import retrofit2.Call
 import retrofit2.http.GET
 import java.util.*
 
 interface RetrofitService {
 
    @GET("posts")
-   fun getAllPosts(): Observable<List<Post>>
+   fun getAllPosts(): Call<List<Post>>
 }
 
